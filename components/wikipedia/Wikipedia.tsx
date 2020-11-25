63990b49dcc219058b66a9f0ca15267bcfc186eb
lrusso96 <russo.1699981@studenti.uniroma1.it>
2020-11-25 19:53:07 +0100

    Wikipedia Card: enhanced design

--- components/wikipedia/Wikipedia.tsx
+++ components/wikipedia/Wikipedia.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import ReadMeImg from '../ReadMeImg';
-import Text from '../Text';
 
 export interface Props {
     edit_count: number;
@@ -18,49 +17,96 @@ export const WikipediaStats: React.FC<Props> = ({
     moved_count,
     user_group,
 }) => {
+
     return (
         <ReadMeImg
-            width="400"
-            height="140">
-            <Text
-                className="title"
-                weight="bold"
-                size="title"
-                color="grey">
-                IT Wikipedia Stats
-            </Text>
-            <table id='icon-wrapper'><tbody>
-                <tr>
-                    <td><strong>User group: </strong></td>
-                    <td>{user_group}</td>
-                </tr>
-                <tr>
-                    <td><strong>Total edits:</strong></td>
-                    <td>{edit_count}</td>
-                </tr>
-                <tr>
-                    <td><strong>Patrol:</strong></td>
-                    <td>{patrol_count}</td>
-                </tr>
-                <tr>
-                    <td><strong>Moves:</strong></td>
-                    <td>{moved_count}</td>
-                </tr>
-                <tr>
-                    <td><strong>Thank:</strong></td>
-                    <td>{thank_count}</td>
-                </tr>
-            </tbody></table>
-            <style>{`
-          #icon-wrapper {
+            width="420"
+            height="220">
+            <div id="wrapper">
+                <svg
+                    version="1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 256"
+                    height="210"
+                    width="410">
+
+                    <text x="39.257812"
+                        y="73.007294"
+                        style={{ fontStyle: "normal", fontVariant: "normal", fontWeight: "bold", fontSize: "23.8933px", lineHeight: "1.25", fontFamily: "Palatino Linotype", fill: "#000000", fillOpacity: 1, stroke: "none" }}>Total Edits:
+        </text>
+
+                    <text x="39.257812"
+                        y="73.007294"
+                        transform="translate(0,32)"
+                        style={{ fontStyle: "normal", fontVariant: "normal", fontWeight: "bold", fontSize: "23.8933px", lineHeight: "1.25", fontFamily: "Palatino Linotype", fill: "#000000", fillOpacity: 1, stroke: "none" }}>Patrol:
+        </text>
+
+                    <text x="39.257812"
+                        y="73.007294"
+                        transform="translate(0,64)"
+                        style={{ fontStyle: "normal", fontVariant: "normal", fontWeight: "bold", fontSize: "23.8933px", lineHeight: "1.25", fontFamily: "Palatino Linotype", fill: "#000000", fillOpacity: 1, stroke: "none" }}>Thank:
+        </text>
+
+                    <text x="39.257812"
+                        y="73.007294"
+                        transform="translate(0,96)"
+                        style={{ fontStyle: "normal", fontVariant: "normal", fontWeight: "bold", fontSize: "23.8933px", lineHeight: "1.25", fontFamily: "Palatino Linotype", fill: "#000000", fillOpacity: 1, stroke: "none" }}>Moves:
+        </text>
+
+                    <text x="39.257812"
+                        y="73.007294"
+                        transform="translate(0,128)"
+                        style={{ fontStyle: "normal", fontVariant: "normal", fontWeight: "bold", fontSize: "23.8933px", lineHeight: "1.25", fontFamily: "Palatino Linotype", fill: "#000000", fillOpacity: 1, stroke: "none" }}>Group:
+        </text>
+
+                    <text x="39.257812"
+                        y="73.007294"
+                        transform="translate(148.8)"
+                        style={{ fontStyle: "normal", fontVariant: "normal", fontWeight: "normal", fontSize: "23.8933px", lineHeight: "1.25", fontFamily: "Palatino Linotype", fill: "#000000", fillOpacity: 1, stroke: "none" }}>{edit_count}
+                    </text>
+
+                    <text x="39.257812"
+                        y="73.007294"
+                        transform="translate(148.8, 32)"
+                        style={{ fontStyle: "normal", fontVariant: "normal", fontWeight: "normal", fontSize: "23.8933px", lineHeight: "1.25", fontFamily: "Palatino Linotype", fill: "#000000", fillOpacity: 1, stroke: "none" }}>{patrol_count}
+                    </text>
+
+                    <text x="39.257812"
+                        y="73.007294"
+                        transform="translate(148.8, 64)"
+                        style={{ fontStyle: "normal", fontVariant: "normal", fontWeight: "normal", fontSize: "23.8933px", lineHeight: "1.25", fontFamily: "Palatino Linotype", fill: "#000000", fillOpacity: 1, stroke: "none" }}>{thank_count}
+                    </text>
+
+                    <text x="39.257812"
+                        y="73.007294"
+                        transform="translate(148.8, 96)"
+                        style={{ fontStyle: "normal", fontVariant: "normal", fontWeight: "normal", fontSize: "23.8933px", lineHeight: "1.25", fontFamily: "Palatino Linotype", fill: "#000000", fillOpacity: 1, stroke: "none" }}>{moved_count}
+                    </text>
+
+                    <text x="39.257812"
+                        y="73.007294"
+                        transform="translate(148.8, 128)"
+                        style={{ fontStyle: "normal", fontVariant: "normal", fontWeight: "normal", fontSize: "23.8933px", lineHeight: "1.25", fontFamily: "Palatino Linotype", fill: "#000000", fillOpacity: 1, stroke: "none" }}>{user_group}
+                    </text>
+
+                    <text x="39.257812"
+                        y="73.007294"
+                        transform="translate(280, 120)"
+                        style={{ fontStyle: "normal", fontVariant: "normal", fontWeight: "normal", fontSize: "200px", lineHeight: "1.25", fontFamily: "Linux Libertine Capitals", fill: "#000000", fillOpacity: 1, stroke: "none" }}>W
+        </text>
+                </svg >
+            </div>
+            <style>
+                {`
+          #wrapper {
             display: flex;
             
             flex-wrap: wrap;
           }
           
-          svg {
-            margin: 12px;
-            padding: 8px;
+          #wrapper svg {
+            margin-left: 8px;
+            margin-top: 8px;
             background: rgb(250, 250, 250);
             box-shadow: 2px 2px 2px rgba(0,0,0,.3);
             border: 1px solid rgba(0,0,0,.01);
